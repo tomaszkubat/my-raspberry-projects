abd75c1ba5a11ece8c6310248299caed92125520
Tomasz <33938125+tomaszkubat@users.noreply.github.com>
2021-11-18 20:04:53 +0100

    addded command to disable wifi

--- setup/setup.sh
+++ setup/setup.sh
@@ -6,3 +6,7 @@ sudo apt-get update && sudo apt-get upgrade
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo usermod -aG docker Pi
+
+
+# disable WiFi
+echo -e "dtoverlay=disable-wifi\ndtoverlay=disable-bt" >> /boot/config.txt
