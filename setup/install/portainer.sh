c28378a67895706ea10775700d6dc8c5dc346cd2
Tomasz <33938125+tomaszkubat@users.noreply.github.com>
2022-04-20 12:10:19 +0200

    Create portainer.sh

---
+++ setup/install/portainer.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# As a prerequisite Docker should be installed first.
+#
+# detailed information about Portainer installation could be found here:
+# https://www.wundertech.net/portainer-raspberry-pi-install-how-to-install-docker-and-portainer/
+
+sudo docker pull portainer/portainer-ce:linux-arm
+
+sudo docker run --restart always --name portainer -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:linux-arm
