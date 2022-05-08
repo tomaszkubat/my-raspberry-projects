b229f4fdf3a0030aa71c2f52836305db562ec316
Tomasz <33938125+tomaszkubat@users.noreply.github.com>
2022-05-08 22:12:04 +0200

    Update portainer.sh

--- setup/portainer.sh
+++ setup/portainer.sh
@@ -7,4 +7,4 @@
 
 sudo docker pull portainer/portainer:linux-arm
 
-sudo docker run --restart always -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer:linux-arm-v portainer_data:/data portainer/portainer-ce:linux-arm
+sudo docker run --restart always -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer:linux-arm -v portainer_data:/data portainer/portainer-ce:linux-arm
