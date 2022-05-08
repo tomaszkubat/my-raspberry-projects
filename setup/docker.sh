3f804d98343d2e40bc23020d05ae805527887c1f
Tomasz <33938125+tomaszkubat@users.noreply.github.com>
2022-05-08 22:06:12 +0200

    Update docker.sh

--- setup/docker.sh
+++ setup/docker.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-# detailed information about Docker installation could be found here:
-# https://www.wundertech.net/portainer-raspberry-pi-install-how-to-install-docker-and-portainer/
+# https://dev.to/elalemanyo/how-to-install-docker-and-docker-compose-on-raspberry-pi-1mo
 
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
-sudo usermod -aG docker Pi
+sudo apt-get update && sudo apt-get upgrade
+
+# install Docker
+curl -sSL https://get.docker.com | sh
+
+# add a non-root user (current user) to the Docker Group
+sudo usermod -aG docker ${USER}
+
+# Install Docker-Compose
+sudo apt-get install libffi-dev libssl-dev
+sudo apt install python3-dev
+sudo apt-get install -y python3 python3-pip
+
+sudo pip3 install docker-compose
+
+sudo systemctl enable docker
