327f118bf5db8c7cc4a45abc0640e266fd89cef4
Tomasz <33938125+tomaszkubat@users.noreply.github.com>
2022-05-08 22:09:43 +0200

    Update portainer.sh

--- setup/portainer.sh
+++ setup/portainer.sh
@@ -5,6 +5,6 @@
 # detailed information about Portainer installation could be found here:
 # https://www.wundertech.net/portainer-raspberry-pi-install-how-to-install-docker-and-portainer/
 
-sudo docker pull portainer/portainer-ce:linux-arm
+sudo docker pull portainer/portainer:linux-arm
 
-sudo docker run --restart always --name portainer -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:linux-arm
+sudo docker run --restart always -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer:linux-arm-v portainer_data:/data portainer/portainer-ce:linux-arm
